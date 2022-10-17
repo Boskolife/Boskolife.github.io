7a03a4a3059b40edf13c15a6e8f058180b20ac4a
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-10-18 00:56:57 +0300

    added one responcive for main sec

--- ApDol/src/js/main.js
+++ ApDol/src/js/main.js
@@ -11,21 +11,33 @@ let msec = document.getElementById('msec');
 
 window.addEventListener('scroll', function(){
   let value = window.scrollY;
+  let laptop = window.matchMedia("(min-width: 1440px)");
   let widescreen = window.matchMedia("(min-width: 1200px)");
   let tablet = window.matchMedia("(min-width: 768px)");
   let phone = window.matchMedia("(min-width: 480px)");
 
-  if (widescreen.matches) { 
+  if (laptop.matches) { 
     m2.style.top = 475 + value * 0.05 + 'px';
     m3.style.top = 390 + value * -0.3 + 'px';
     cloud1.style.left = -60 + value * -0.3 + 'px';
     cloud2.style.left = -100+ value * -0.3 + 'px';
     cloud3.style.right = -300 + value * -0.3 + 'px';
-    cloud1.style.bottom = 340 + value * 0.4 + 'px';
-    cloud2.style.bottom = -325 + value * 0.4 + 'px';
-    cloud3.style.bottom = -15 + value * 0.4 + 'px';
+    cloud1.style.bottom = 340 + value * 0.2 + 'px';
+    cloud2.style.bottom = -325 + value * 0.2 + 'px';
+    cloud3.style.bottom = -15 + value * 0.2 + 'px';
     text.style.top = 415 + value * 0.3 + 'px';
-    msec.style.minHeight = 1830 + value * -0.3 + 'px';
+    msec.style.minHeight = 1785 + value * -0.3 + 'px';
+  }  else if (widescreen.matches) { 
+    m2.style.top = 350 + value * 0.05 + 'px';
+    m3.style.top = 200 + value * -0.3 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100+ value * -0.3 + 'px';
+    cloud3.style.right = -300 + value * -0.3 + 'px';
+    cloud1.style.bottom = 240 + value * 0.1 + 'px';
+    cloud2.style.bottom = -240 + value * 0.1 + 'px';
+    cloud3.style.bottom = -15 + value * 0.1 + 'px';
+    text.style.top = 220 + value * 0.3 + 'px';
+    msec.style.minHeight = 1205 + value * -0.3 + 'px';
   }  else if (tablet.matches) {
     m2.style.top = 265 + value * 0.05 + 'px';
     m3.style.top = 100 + value * -0.2 + 'px';
