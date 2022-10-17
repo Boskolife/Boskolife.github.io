03fc4a99666a00f4ec17e780c464a164d59d1ee5
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-10-18 00:31:04 +0300

    added styles for main sections

--- ApDol/dist/js/main.js
+++ ApDol/dist/js/main.js
@@ -1,40 +1,64 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+/* paralax */
+var m2 = document.getElementById('m2');
+var m3 = document.getElementById('m3');
+var cloud1 = document.getElementById('cloud1');
+var cloud2 = document.getElementById('cloud2');
+var cloud3 = document.getElementById('cloud3');
+var text = document.getElementById('text');
+var msec = document.getElementById('msec');
+window.addEventListener('scroll', function () {
+  var value = window.scrollY;
+  var widescreen = window.matchMedia("(min-width: 1200px)");
+  var tablet = window.matchMedia("(min-width: 768px)");
+  var phone = window.matchMedia("(min-width: 480px)");
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-// Swiper:
-function destroySlidersOnResize(selector, width, obj, moreThan) {
-  var init = _objectSpread({}, obj);
-
-  var win = window;
-  var sliderSelector = document.querySelector(selector);
-  var swiper = new Swiper(selector, init);
-
-  var toggleInit = function toggleInit() {
-    var neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width;
-
-    if (neededWidth) {
-      if (!sliderSelector.classList.contains("swiper-initialized")) {
-        swiper = new Swiper(selector, init);
-      }
-    } else if (sliderSelector.classList.contains("swiper-initialized")) {
-      swiper.destroy();
-    }
-  };
-
-  ["load", "resize"].forEach(function (evt) {
-    return win.addEventListener(evt, toggleInit, false);
-  });
-}
-
-destroySlidersOnResize(".me-slider", 960, {
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination"
+  if (widescreen.matches) {
+    m2.style.top = 475 + value * 0.05 + 'px';
+    m3.style.top = 390 + value * -0.3 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100 + value * -0.3 + 'px';
+    cloud3.style.right = -300 + value * -0.3 + 'px';
+    cloud1.style.bottom = 340 + value * 0.4 + 'px';
+    cloud2.style.bottom = -325 + value * 0.4 + 'px';
+    cloud3.style.bottom = -15 + value * 0.4 + 'px';
+    text.style.top = 415 + value * 0.3 + 'px';
+    msec.style.minHeight = 1830 + value * -0.3 + 'px';
+  } else if (tablet.matches) {
+    m2.style.top = 265 + value * 0.05 + 'px';
+    m3.style.top = 100 + value * -0.2 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100 + value * -0.3 + 'px';
+    cloud3.style.right = -300 + value * -0.3 + 'px';
+    cloud1.style.bottom = 240 + value * 0.1 + 'px';
+    cloud2.style.bottom = -240 + value * 0.1 + 'px';
+    cloud3.style.bottom = -40 + value * 0.1 + 'px';
+    text.style.top = 120 + value * 0.3 + 'px';
+    msec.style.minHeight = 940 + value * -0.2 + 'px';
+  } else if (phone.matches) {
+    m2.style.top = 185 + value * 0.05 + 'px';
+    m3.style.top = 100 + value * -0.2 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100 + value * -0.3 + 'px';
+    cloud3.style.right = -100 + value * -0.3 + 'px';
+    cloud1.style.bottom = 80 + value * 0.1 + 'px';
+    cloud2.style.bottom = -140 + value * 0.1 + 'px';
+    cloud3.style.bottom = -40 + value * 0.1 + 'px';
+    text.style.top = 160 + value * 0.3 + 'px';
+    msec.style.minHeight = 640 + value * -0.2 + 'px';
+  } else {
+    m2.style.top = 185 + value * 0.05 + 'px';
+    m3.style.top = 100 + value * -0.2 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100 + value * -0.3 + 'px';
+    cloud3.style.right = -100 + value * -0.3 + 'px';
+    cloud1.style.bottom = 80 + value * 0.1 + 'px';
+    cloud2.style.bottom = -70 + value * 0.1 + 'px';
+    cloud3.style.bottom = value * 0.1 + 'px';
+    text.style.top = 160 + value * 0.3 + 'px';
+    msec.style.minHeight = 640 + value * -0.2 + 'px';
   }
 });
+/* ///// */
 //# sourceMappingURL=main.js.map
--- ApDol/src/js/main.js
+++ ApDol/src/js/main.js
@@ -1,35 +1,66 @@
-// Swiper:
+/* paralax */
 
-function destroySlidersOnResize(selector, width, obj, moreThan) {
-    const init = {
-      ...obj,
-    };
-  
-    const win = window;
-    const sliderSelector = document.querySelector(selector);
-    let swiper = new Swiper(selector, init);
-  
-    const toggleInit = () => {
-      const neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width
-      if (neededWidth) {
-        if (!sliderSelector.classList.contains("swiper-initialized")) {
-          swiper = new Swiper(selector, init);
-        }
-      } else if (sliderSelector.classList.contains("swiper-initialized")) {
-        swiper.destroy();
-      }
-    };
-  
-    ["load", "resize"].forEach((evt) =>
-      win.addEventListener(evt, toggleInit, false)
-    );
-}
+let m2 = document.getElementById('m2');
+let m3 = document.getElementById('m3');
+let cloud1 = document.getElementById('cloud1');
+let cloud2 = document.getElementById('cloud2');
+let cloud3 = document.getElementById('cloud3');
+let text = document.getElementById('text');
+let msec = document.getElementById('msec');
 
-destroySlidersOnResize(".me-slider", 960, {
-    spaceBetween: 20,
-  
-    pagination: {
-      el: ".swiper-pagination",
-    },
-});
-  
+
+window.addEventListener('scroll', function(){
+  let value = window.scrollY;
+  let widescreen = window.matchMedia("(min-width: 1200px)");
+  let tablet = window.matchMedia("(min-width: 768px)");
+  let phone = window.matchMedia("(min-width: 480px)");
+
+  if (widescreen.matches) { 
+    m2.style.top = 475 + value * 0.05 + 'px';
+    m3.style.top = 390 + value * -0.3 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100+ value * -0.3 + 'px';
+    cloud3.style.right = -300 + value * -0.3 + 'px';
+    cloud1.style.bottom = 340 + value * 0.4 + 'px';
+    cloud2.style.bottom = -325 + value * 0.4 + 'px';
+    cloud3.style.bottom = -15 + value * 0.4 + 'px';
+    text.style.top = 415 + value * 0.3 + 'px';
+    msec.style.minHeight = 1830 + value * -0.3 + 'px';
+  }  else if (tablet.matches) {
+    m2.style.top = 265 + value * 0.05 + 'px';
+    m3.style.top = 100 + value * -0.2 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100+ value * -0.3 + 'px';
+    cloud3.style.right = -300 + value * -0.3 + 'px';
+    cloud1.style.bottom = 240 + value * 0.1 + 'px';
+    cloud2.style.bottom = -240 + value * 0.1 + 'px';
+    cloud3.style.bottom = -40 + value * 0.1 + 'px';
+    text.style.top = 120 + value * 0.3 + 'px';
+    msec.style.minHeight = 940 + value * -0.2 + 'px';
+  } else if (phone.matches) {
+    m2.style.top = 185 + value * 0.05 + 'px';
+    m3.style.top = 100 + value * -0.2 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100+ value * -0.3 + 'px';
+    cloud3.style.right = -100 + value * -0.3 + 'px';
+    cloud1.style.bottom = 80 + value * 0.1 + 'px';
+    cloud2.style.bottom = -140 + value * 0.1 + 'px';
+    cloud3.style.bottom = -40 + value * 0.1 + 'px';
+    text.style.top = 160 + value * 0.3 + 'px';
+    msec.style.minHeight = 640 + value * -0.2 + 'px';
+  } else {
+    m2.style.top = 185 + value * 0.05 + 'px';
+    m3.style.top = 100 + value * -0.2 + 'px';
+    cloud1.style.left = -60 + value * -0.3 + 'px';
+    cloud2.style.left = -100+ value * -0.3 + 'px';
+    cloud3.style.right = -100 + value * -0.3 + 'px';
+    cloud1.style.bottom = 80 + value * 0.1 + 'px';
+    cloud2.style.bottom = -70 + value * 0.1 + 'px';
+    cloud3.style.bottom = value * 0.1 + 'px';
+    text.style.top = 160 + value * 0.3 + 'px';
+    msec.style.minHeight = 640 + value * -0.2 + 'px';
+  }
+})
+
+
+/* ///// */
