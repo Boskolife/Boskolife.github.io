753892af6bca4ac733d4a65d3240aae095e940d2
Boskolife <boskoboskoboskobosko@gmail.com>
2022-11-29 17:39:32 +0200

    styles tabs and content to tabs

--- Real Projects/Him-Paritet/src/js/main.js
+++ Real Projects/Him-Paritet/src/js/main.js
@@ -1,39 +1,3 @@
-// Swiper:
-
-function destroySlidersOnResize(selector, width, obj, moreThan) {
-    const init = {
-      ...obj,
-    };
-  
-    const win = window;
-    const sliderSelector = document.querySelector(selector);
-    let swiper = new Swiper(selector, init);
-  
-    const toggleInit = () => {
-      const neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width
-      if (neededWidth) {
-        if (!sliderSelector.classList.contains("swiper-initialized")) {
-          swiper = new Swiper(selector, init);
-        }
-      } else if (sliderSelector.classList.contains("swiper-initialized")) {
-        swiper.destroy();
-      }
-    };
-  
-    ["load", "resize"].forEach((evt) =>
-      win.addEventListener(evt, toggleInit, false)
-    );
-}
-
-destroySlidersOnResize(".me-slider", 960, {
-    spaceBetween: 20,
-  
-    pagination: {
-      el: ".swiper-pagination",
-    },
-});
-
-
 
 const burger = document.querySelector(".burger_menu");
 const menuBody = document.querySelector(".nav");
