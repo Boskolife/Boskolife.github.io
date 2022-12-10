ac3d55f4581a2854216eca18db12904282df86c6
yzhura <zhuravlev.yaroslav@gmail.com>
2022-12-10 02:00:50 +0200

    dist

--- Real Projects/Cash Trinity/dist/js/main.js
+++ Real Projects/Cash Trinity/dist/js/main.js
@@ -1,5 +1,17 @@
 "use strict";
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
@@ -12,6 +24,7 @@ function destroySlidersOnResize(selector, width, obj, moreThan) {
 
   var win = window;
   var sliderSelector = document.querySelector(selector);
+  if (!sliderSelector) return;
   var swiper = new Swiper(selector, init);
 
   var toggleInit = function toggleInit() {
@@ -34,17 +47,17 @@ function destroySlidersOnResize(selector, width, obj, moreThan) {
 destroySlidersOnResize(".lending_slider", 9999, {
   spaceBetween: 20,
   sliderPerView: 1,
-  direction: 'horizontal',
+  direction: "horizontal",
   mousewheel: {
     sensitivity: 1
   },
   pagination: {
     el: ".swiper-pagination",
-    type: 'progressbar'
+    type: "progressbar"
   },
   breakpoints: {
     768: {
-      direction: 'vertical',
+      direction: "vertical",
       autoHeght: true
     }
   }
@@ -61,8 +74,6 @@ if (burger) {
   });
 }
 
-;
-
 if (linkClose.length) {
   for (var i = 0; i < linkClose.length; ++i) {
     linkClose[i].addEventListener("click", function (e) {
@@ -73,12 +84,19 @@ if (linkClose.length) {
   }
 }
 
-;
-new CircleType(document.getElementById('simple_arc'));
+try {
+  new CircleType(document.getElementById("simple_arc"));
+} catch (err) {
+  console.log("err: ", err);
+}
+
+initHeader();
 initCustomSlider();
+initChart();
 
 function initCustomSlider() {
   var form = document.querySelector("#investForm");
+  if (!form) return;
   var sliderWrap = document.querySelector(".invest-range");
   var htmlRange = sliderWrap.querySelector('input[type="range"]');
   var slider = form.querySelector("#slider");
@@ -90,7 +108,7 @@ function initCustomSlider() {
 
   noUiSlider.create(slider, {
     start: 5,
-    connect: true,
+    connect: "lower",
     range: {
       min: 5,
       max: 2000
@@ -106,5 +124,201 @@ function initCustomSlider() {
     htmlRange.value = value;
     result.textContent = "$ ".concat(formatNumber(value));
   });
+}
+
+function initHeader() {
+  var header = document.querySelector("#header");
+
+  var sections = _toConsumableArray(document.querySelectorAll(".transparent-header"));
+
+  if (!header || !sections) return;
+  var sectionsPositions = sections.map(function (section) {
+    var sectionStartPos = section.offsetTop;
+    var sectionHeight = section.offsetHeight;
+    var sectionEnd = sectionStartPos + sectionHeight;
+    return {
+      start: sectionStartPos,
+      end: sectionEnd
+    };
+  });
+  document.addEventListener("scroll", function () {
+    var currentScrollPosY = window.scrollY;
+    var isHeaderOverlaySection = sectionsPositions.find(function (pos) {
+      return currentScrollPosY >= pos.start && currentScrollPosY <= pos.end - header.offsetHeight;
+    });
+    isHeaderOverlaySection ? header.classList.add("transparent") : header.classList.remove("transparent");
+  });
+}
+
+function initChart() {
+  var chartContainer = document.querySelector("#chart");
+  if (!chartContainer) return;
+  var chart;
+  var onlyDigitsRgx = /^\d+$/;
+  var MAX_YEARS = 50;
+  var MIN_YEARS = 1;
+  var investmentInput = document.querySelector("#investmentInput");
+  var onceRadio = document.querySelector("#onceRadio");
+  var weeklyRadio = document.querySelector("#weeklyRadio");
+  var monthlyRadio = document.querySelector("#monthlyRadio");
+  var annuallyRadio = document.querySelector("#annuallyRadio");
+  var timeInvest = 1;
+  var RATE = 13;
+  var numberOfYearsInput = document.querySelector("#numberOfYearsInput");
+  investmentInput.addEventListener("input", function (event) {
+    var value = event.target.value;
+
+    if (!onlyDigitsRgx.test(value)) {
+      event.target.value = "";
+      return;
+    }
+
+    draw();
+  });
+  numberOfYearsInput.addEventListener("input", function (event) {
+    var value = event.target.value;
+
+    if (!onlyDigitsRgx.test(value)) {
+      event.target.value = "";
+      return;
+    }
+  });
+  numberOfYearsInput.addEventListener("blur", function (event) {
+    var value = event.target.value;
+
+    if (+value > MAX_YEARS) {
+      event.target.value = MAX_YEARS;
+    }
+
+    if (+value < MIN_YEARS) {
+      event.target.value = MIN_YEARS;
+    }
+
+    draw();
+  });
+  document.addEventListener("click", function (event) {
+    // TODO: formula
+    switch (event.target) {
+      case onceRadio:
+        timeInvest = 2;
+        draw();
+        break;
+
+      case weeklyRadio:
+        timeInvest = 2.5;
+        draw();
+        break;
+
+      case monthlyRadio:
+        timeInvest = 3;
+        draw();
+        break;
+
+      case annuallyRadio:
+        timeInvest = 4;
+        draw();
+        break;
+
+      default:
+        break;
+    }
+  });
+  draw();
+
+  function draw() {
+    // TODO: formula
+    var interest = new Array(+numberOfYearsInput.value + 1).fill(0).map(function (_, ind) {
+      var res = +investmentInput.value * +numberOfYearsInput.value * timeInvest / RATE * ind;
+      return +res.toFixed();
+    }); // TODO: do we need two more?
+
+    var savings = [];
+    var pv = [];
+    chart = new Highcharts.Chart({
+      chart: {
+        renderTo: "chart",
+        type: "column",
+        height: 400
+      },
+      colors: ["#3772FF", "#4C7DF8", "#0146F5", "#BDF0F4", "#42BFC7", "#1F1247"],
+      credits: {
+        enabled: false
+      },
+      legend: {
+        enabled: false
+      },
+      title: {
+        text: null
+      },
+      xAxis: {
+        min: 1,
+        title: {
+          text: "Years",
+          style: {
+            color: "#777E90"
+          }
+        } // allowDecimals: false,
+
+      },
+      yAxis: {
+        title: {
+          text: "Balance",
+          style: {
+            color: "#777E90"
+          }
+        } // endOnTick: false,
+
+      },
+      plotOptions: {
+        column: {
+          stacking: "normal",
+          borderRadius: 4,
+          dataLabels: {
+            enabled: false
+          }
+        },
+        series: {
+          borderWidth: 0,
+          shadow: false,
+          groupPadding: 0.1,
+          pointPadding: 0.05
+        }
+      },
+      // TODO: tooltip?
+      // tooltip: {
+      //   shared: true,
+      //   useHTML: true,
+      //   borderColor: "#42BFC7",
+      //   formatter: function () {
+      //     var s = this.x;
+      //     if (s == 1) {
+      //       s = "year";
+      //     } else {
+      //       s = "years";
+      //     }
+      //     return s;
+      //   },
+      // },
+      tooltip: {
+        enabled: false
+      },
+      series: [{
+        name: "Interest",
+        data: interest,
+        stack: "original",
+        legendIndex: 3
+      }, {
+        name: "Regular deposits",
+        data: savings,
+        stack: "original",
+        legendIndex: 2
+      }, {
+        name: "Initial deposit",
+        data: pv,
+        stack: "original",
+        legendIndex: 1
+      }]
+    });
+  }
 }
 //# sourceMappingURL=main.js.map
