71086fc5d9cce238401bc6f0cba017a8d9c7ded6
Boskolife <boskoboskoboskobosko@gmail.com>
2022-12-11 16:47:36 +0200

    add style to active nav menu

--- Real Projects/Cash Trinity/src/js/main.js
+++ Real Projects/Cash Trinity/src/js/main.js
@@ -446,3 +446,41 @@ function initChart() {
     });
   }
 }
+
+
+//Setting Active States on Sticky Navigations while Scrolling
+
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="index.html#'+$(this).attr('id')+'"]').addClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="calculator.html#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
