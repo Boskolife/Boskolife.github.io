77cf0f91f227394c22bf4d5b128ae2bc7a615ee0
yzhura <zhuravlev.yaroslav@gmail.com>
2022-12-11 14:39:35 +0200

    Update main.js

--- Real Projects/Cash Trinity/src/js/main.js
+++ Real Projects/Cash Trinity/src/js/main.js
@@ -1,4 +1,53 @@
-// Swiper:
+initCurve();
+initHeader();
+initCustomSlider();
+initChart();
+initCircleBtn();
+initBurgerMenu();
+
+destroySlidersOnResize(".lending_slider", 9999, {
+  spaceBetween: 20,
+  sliderPerView: 1,
+  direction: "horizontal",
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+
+  breakpoints: {
+    768: {
+      direction: "vertical",
+      autoHeght: true,
+    },
+  },
+});
+
+function initBurgerMenu() {
+  const burger = document.querySelector(".burger_menu");
+  const menuBody = document.querySelector(".nav");
+  const linkClose = document.querySelectorAll(".link-close");
+  if (burger) {
+    burger.addEventListener("click", function (e) {
+      document.body.classList.toggle("body_lock");
+      burger.classList.toggle("burger_active");
+      menuBody.classList.toggle("menu_active");
+    });
+  }
+  
+  if (linkClose.length) {
+    for (var i = 0; i < linkClose.length; ++i) {
+      linkClose[i].addEventListener("click", function (e) {
+        document.body.classList.remove("body_lock");
+        burger.classList.remove("burger_active");
+        menuBody.classList.remove("menu_active");
+      });
+    }
+  }
+}
 
 function destroySlidersOnResize(selector, width, obj, moreThan) {
   const init = {
@@ -29,54 +78,6 @@ function destroySlidersOnResize(selector, width, obj, moreThan) {
     win.addEventListener(evt, toggleInit, false)
   );
 }
-
-destroySlidersOnResize(".lending_slider", 9999, {
-  spaceBetween: 20,
-  sliderPerView: 1,
-  direction: "horizontal",
-  mousewheel: {
-    sensitivity: 1,
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-  },
-
-  breakpoints: {
-    768: {
-      direction: "vertical",
-      autoHeght: true,
-    },
-  },
-});
-
-const burger = document.querySelector(".burger_menu");
-const menuBody = document.querySelector(".nav");
-const linkClose = document.querySelectorAll(".link-close");
-if (burger) {
-  burger.addEventListener("click", function (e) {
-    document.body.classList.toggle("body_lock");
-    burger.classList.toggle("burger_active");
-    menuBody.classList.toggle("menu_active");
-  });
-}
-
-if (linkClose.length) {
-  for (var i = 0; i < linkClose.length; ++i) {
-    linkClose[i].addEventListener("click", function (e) {
-      document.body.classList.remove("body_lock");
-      burger.classList.remove("burger_active");
-      menuBody.classList.remove("menu_active");
-    });
-  }
-}
-
-initCurve();
-initHeader();
-initCustomSlider();
-initChart();
-initCircleBtn();
 
 function initCurve() {
   const curveTextWrap = document.querySelector(".curve-text-wrap");
