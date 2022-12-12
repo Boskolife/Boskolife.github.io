dc9571af298b6d34964ffaa71be1d69b60039051
Boskolife <boskoboskoboskobosko@gmail.com>
2022-12-12 15:17:05 +0200

    init wow

--- Real Projects/Cash Trinity/dist/js/main.js
+++ Real Projects/Cash Trinity/dist/js/main.js
@@ -24,8 +24,7 @@ initCustomSlider();
 initChart();
 initCircleBtn();
 initBurgerMenu();
-/* new WOW().init();  */
-
+new WOW().init();
 destroySlidersOnResize(".lending_slider", 9999, {
   spaceBetween: 20,
   sliderPerView: 1,
