f8936c51fa8e3826b6990730fc79db53c89b36c1
yzhura <zhuravlev.yaroslav@gmail.com>
2022-12-12 23:01:28 +0200

    dist

--- Real Projects/Cash Trinity/dist/js/main.js
+++ Real Projects/Cash Trinity/dist/js/main.js
@@ -243,10 +243,10 @@ function initChart() {
     }
 
     if (!isAnnually) {
-      return +originalInvest * (+timeInvest * +numberOfYearsInputValue) + +originalInvest;
+      return +originalInvest * (+timeInvest * +numberOfYearsInputValue);
     }
 
-    return +originalInvest * +numberOfYearsInputValue + +originalInvest;
+    return +originalInvest * +numberOfYearsInputValue;
   };
 
   investmentInput.addEventListener("input", function (event) {
