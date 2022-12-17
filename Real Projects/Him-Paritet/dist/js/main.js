b44bd0fbe66d4189151fe0495a6ab9e7beb2b7d3
yzhura <zhuravlev.yaroslav@gmail.com>
2022-12-17 03:35:26 +0200

    dist tab navigation

--- Real Projects/Him-Paritet/dist/js/main.js
+++ Real Projects/Him-Paritet/dist/js/main.js
@@ -12,14 +12,30 @@ var menuBody = document.querySelector(".nav");
 var linkClose = document.querySelectorAll(".link-close");
 
 if (burger) {
-  burger.addEventListener("click", function (e) {
+  burger.addEventListener("click", function () {
     document.body.classList.toggle("body_lock");
     burger.classList.toggle("burger_active");
     menuBody.classList.toggle("menu_active");
+    var subMenu = document.querySelector(".menu-item_active");
+    var arrowTabWrap = document.querySelector(".arrow_tab_wrap_active");
+    var arrowOpen = document.querySelector(".arrow_open");
+    if (subMenu) subMenu.classList.remove("menu-item_active");
+    if (arrowTabWrap) arrowTabWrap.classList.remove("arrow_tab_wrap_active");
+    if (arrowOpen) arrowOpen.classList.remove("arrow_open");
   });
 }
 
-;
+function closeBurgerMenu() {
+  document.body.classList.remove("body_lock");
+  burger.classList.remove("burger_active");
+  menuBody.classList.remove("menu_active");
+  var subMenu = document.querySelector(".menu-item_active");
+  var arrowTabWrap = document.querySelector(".arrow_tab_wrap_active");
+  var arrowOpen = document.querySelector(".arrow_open");
+  if (subMenu) subMenu.classList.remove("menu-item_active");
+  if (arrowTabWrap) arrowTabWrap.classList.remove("arrow_tab_wrap_active");
+  if (arrowOpen) arrowOpen.classList.remove("arrow_open");
+}
 
 if (linkClose.length) {
   for (var i = 0; i < linkClose.length; ++i) {
@@ -29,9 +45,8 @@ if (linkClose.length) {
       menuBody.classList.remove("menu_active");
     });
   }
-}
+} // Category btn
 
-; // Category btn
 
 var category = document.querySelector(".category_btn");
 var tabsBody = document.querySelector(".tabs__nav");
@@ -45,8 +60,6 @@ if (category) {
   });
 }
 
-;
-
 if (tabClose.length) {
   for (var i = 0; i < tabClose.length; ++i) {
     tabClose[i].addEventListener("click", function (e) {
@@ -55,9 +68,8 @@ if (tabClose.length) {
       tabsBody.classList.remove("tabs__nav_active");
     });
   }
-}
+} // Click arrow
 
-; // Click arrow
 
 window.onload = function () {
   document.addEventListener("click", documentActions);
@@ -66,10 +78,10 @@ window.onload = function () {
     var targetElement = e.target;
 
     if (window.innerWidth < 768) {
-      if (targetElement.classList.contains('arrow')) {
-        targetElement.closest('.menu-item').classList.toggle('menu-item_active');
-        targetElement.closest('.arrow_tab_wrap').classList.toggle('arrow_tab_wrap_active');
-        targetElement.closest('.arrow').classList.toggle('arrow_open');
+      if (targetElement.classList.contains("arrow")) {
+        targetElement.closest(".menu-item").classList.toggle("menu-item_active");
+        targetElement.closest(".arrow_tab_wrap").classList.toggle("arrow_tab_wrap_active");
+        targetElement.closest(".arrow").classList.toggle("arrow_open");
       }
     }
   }
@@ -78,14 +90,21 @@ window.onload = function () {
 
 var ItcTabs = /*#__PURE__*/function () {
   function ItcTabs(target, config) {
+    var _this$_elTabs, _this$_elTabs2;
+
     _classCallCheck(this, ItcTabs);
 
     var defaultConfig = {};
     this._config = Object.assign(defaultConfig, config);
-    this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
-    this._elButtons = this._elTabs.querySelectorAll('.tabs__btn');
-    this._elPanes = this._elTabs.querySelectorAll('.tabs__pane');
-    this._eventShow = new Event('tab.itc.change');
+    this._elTabs = typeof target === "string" ? document.querySelector(target) : target;
+    this._elButtons = (_this$_elTabs = this._elTabs) === null || _this$_elTabs === void 0 ? void 0 : _this$_elTabs.querySelectorAll(".tabs__btn");
+    this._navElButtons = document.querySelectorAll(".nav_tabs__btn");
+    this._cardElButtons = document.querySelectorAll(".card_link");
+    this._elPanes = (_this$_elTabs2 = this._elTabs) === null || _this$_elTabs2 === void 0 ? void 0 : _this$_elTabs2.querySelectorAll(".tabs__pane");
+    this._eventShow = new Event("tab.itc.change");
+    this._navigationElements = [this._navElButtons];
+    if (this._elButtons) this._navigationElements.push(this._elButtons);
+    if (this._cardElButtons) this._navigationElements.push(this._cardElButtons);
 
     this._init();
 
@@ -95,34 +114,53 @@ var ItcTabs = /*#__PURE__*/function () {
   _createClass(ItcTabs, [{
     key: "_init",
     value: function _init() {
-      var _this = this;
+      if (this._elTabs) this._elTabs.setAttribute("role", "tablist");
 
-      this._elTabs.setAttribute('role', 'tablist');
-
-      this._elButtons.forEach(function (el, index) {
-        el.dataset.index = index;
-        el.setAttribute('role', 'tab');
-
-        _this._elPanes[index].setAttribute('role', 'tabpanel');
+      this._navigationElements.forEach(function (nav) {
+        nav.forEach(function (el, index) {
+          el.dataset.index = index;
+          el.setAttribute("role", "tab");
+        });
       });
+
+      if (this._elPanes) {
+        this._elPanes.forEach(function (tabContent, index) {
+          var _tabContent$index;
+
+          (_tabContent$index = tabContent[index]) === null || _tabContent$index === void 0 ? void 0 : _tabContent$index.setAttribute("role", "tabpanel");
+        });
+      }
+
+      this.showByHash();
+    }
+  }, {
+    key: "showByHash",
+    value: function showByHash() {
+      var _window$location$hash;
+
+      var currentIndexFromUrl = (_window$location$hash = window.location.hash) === null || _window$location$hash === void 0 ? void 0 : _window$location$hash.replace(/#/, "");
+
+      if (currentIndexFromUrl) {
+        this.showByIndex(currentIndexFromUrl);
+      }
     }
   }, {
     key: "show",
     value: function show(elLinkTarget) {
       var elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
 
-      var elLinkActive = this._elTabs.querySelector('.tabs__btn_active');
+      var elLinkActive = this._elTabs.querySelector(".tabs__btn_active");
 
-      var elPaneShow = this._elTabs.querySelector('.tabs__pane_show');
+      var elPaneShow = this._elTabs.querySelector(".tabs__pane_show");
 
       if (elLinkTarget === elLinkActive) {
         return;
       }
 
-      elLinkActive ? elLinkActive.classList.remove('tabs__btn_active') : null;
-      elPaneShow ? elPaneShow.classList.remove('tabs__pane_show') : null;
-      elLinkTarget.classList.add('tabs__btn_active');
-      elPaneTarget.classList.add('tabs__pane_show');
+      elLinkActive ? elLinkActive.classList.remove("tabs__btn_active") : null;
+      elPaneShow ? elPaneShow.classList.remove("tabs__pane_show") : null;
+      elLinkTarget.classList.add("tabs__btn_active");
+      elPaneTarget.classList.add("tabs__pane_show");
 
       this._elTabs.dispatchEvent(this._eventShow);
 
@@ -131,22 +169,37 @@ var ItcTabs = /*#__PURE__*/function () {
   }, {
     key: "showByIndex",
     value: function showByIndex(index) {
-      var elLinkTarget = this._elButtons[index];
+      var elLinkTarget = this._elButtons[+index];
       elLinkTarget ? this.show(elLinkTarget) : null;
     }
   }, {
     key: "_events",
     value: function _events() {
-      var _this2 = this;
+      var _this = this;
 
-      this._elTabs.addEventListener('click', function (e) {
-        var target = e.target.closest('.tabs__btn');
+      document.addEventListener("click", function (e) {
+        var target = e.target.closest(".tabs__btn") || e.target.closest(".nav_tabs__btn") || e.target.closest(".card_link");
 
         if (target) {
+          var isNavLinkFromHeader = target.closest("header");
           e.preventDefault();
+          var index = target.classList.contains("card_link") ? +target.dataset.index + 1 : +target.dataset.index;
 
-          _this2.show(target);
+          if (isNavLinkFromHeader) {
+            closeBurgerMenu();
+          }
+
+          var path = window.location.pathname;
+
+          if (path.includes("our_production")) {
+            _this.show(target);
+          }
+
+          window.location.href = "/dist/our_production.html#".concat(index);
         }
+      });
+      window.addEventListener("locationchange", function () {
+        _this.showByHash();
       });
     }
   }]);
@@ -154,5 +207,29 @@ var ItcTabs = /*#__PURE__*/function () {
   return ItcTabs;
 }();
 
-new ItcTabs('.tabs');
+new ItcTabs(".tabs");
+
+(function () {
+  var oldPushState = history.pushState;
+
+  history.pushState = function pushState() {
+    var ret = oldPushState.apply(this, arguments);
+    window.dispatchEvent(new Event("pushstate"));
+    window.dispatchEvent(new Event("locationchange"));
+    return ret;
+  };
+
+  var oldReplaceState = history.replaceState;
+
+  history.replaceState = function replaceState() {
+    var ret = oldReplaceState.apply(this, arguments);
+    window.dispatchEvent(new Event("replacestate"));
+    window.dispatchEvent(new Event("locationchange"));
+    return ret;
+  };
+
+  window.addEventListener("popstate", function () {
+    window.dispatchEvent(new Event("locationchange"));
+  });
+})();
 //# sourceMappingURL=main.js.map
