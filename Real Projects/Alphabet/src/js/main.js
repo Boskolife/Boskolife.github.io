3b9397dc664073aa18fae2afc39bf85b9736b504
Boskolife <boskoboskoboskobosko@gmail.com>
2023-01-13 07:55:21 +0200

    add new projeckt "Alphabet"

---
+++ Real Projects/Alphabet/src/js/main.js
@@ -0,0 +1,42 @@
+// Swiper:
+
+function destroySlidersOnResize(selector, width, obj, moreThan) {
+    const init = {
+      ...obj,
+    };
+  
+    const win = window;
+    const sliderSelector = document.querySelector(selector);
+    let swiper = new Swiper(selector, init);
+  
+    const toggleInit = () => {
+      const neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width
+      if (neededWidth) {
+        if (!sliderSelector.classList.contains("swiper-initialized")) {
+          swiper = new Swiper(selector, init);
+        }
+      } else if (sliderSelector.classList.contains("swiper-initialized")) {
+        swiper.destroy();
+      }
+    };
+  
+    ["load", "resize"].forEach((evt) =>
+      win.addEventListener(evt, toggleInit, false)
+    );
+}
+
+destroySlidersOnResize(".service_swiper", 9999, {
+    slidesPerView: 1,
+    slidesPerGroup:1,
+    speed:850,
+  
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+  
