e000f4618ce92516fe273b0f6a088fb1b4d27058
Boskolife <boskoboskoboskobosko@gmail.com>
2023-03-11 17:21:15 +0200

    create main page

--- Real Projects/Creditor Solutions/dist/js/main.js
+++ Real Projects/Creditor Solutions/dist/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Burger
 var burger = document.querySelector(".burger_menu");
 var menuBody = document.querySelector(".nav");
 var phone = document.querySelector(".phone");
@@ -7,6 +8,42 @@ burger.addEventListener('click', function () {
   menuBody.classList.toggle('menu_active');
   phone.classList.toggle('menu_active');
   burger.classList.toggle("burger_active");
-  document.body.style.overflow = 'hidden';
+  document.body.classList.toggle("body_lock");
+}); //Tabs
+
+var tabs = document.querySelectorAll('.tab_title'),
+    tabsContent = document.querySelectorAll('.tab_content'),
+    tabsParent = document.querySelector('.tab_wrapper');
+
+function hideTabContent() {
+  tabsContent.forEach(function (item) {
+    item.classList.add('hide');
+    item.classList.remove('show');
+  });
+  tabs.forEach(function (item) {
+    item.classList.remove('tab_active');
+  });
+}
+
+function showTabContent() {
+  var i = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  tabsContent[i].classList.add('show');
+  tabsContent[i].classList.remove('hide');
+  tabs[i].classList.add('tab_active');
+}
+
+hideTabContent();
+showTabContent();
+tabsParent.addEventListener('click', function (event) {
+  var target = event.target;
+
+  if (target && target.classList.contains('tab_title')) {
+    tabs.forEach(function (item, i) {
+      if (target == item) {
+        hideTabContent();
+        showTabContent(i);
+      }
+    });
+  }
 });
 //# sourceMappingURL=main.js.map
--- Real Projects/Creditor Solutions/src/js/main.js
+++ Real Projects/Creditor Solutions/src/js/main.js
@@ -1,4 +1,4 @@
-
+//Burger
 const burger = document.querySelector(".burger_menu");
 const menuBody = document.querySelector(".nav");
 const phone = document.querySelector(".phone");
@@ -7,5 +7,47 @@ burger.addEventListener('click', () =>{
     menuBody.classList.toggle('menu_active');
     phone.classList.toggle('menu_active');
     burger.classList.toggle("burger_active");
-    document.body.style.overflow = 'hidden';
+    document.body.classList.toggle("body_lock");
 });
+
+
+//Tabs
+
+let tabs = document.querySelectorAll('.tab_title'),
+	tabsContent = document.querySelectorAll('.tab_content'),
+	tabsParent = document.querySelector('.tab_wrapper');
+
+	function hideTabContent() {
+        
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tab_active');
+        });
+	}
+
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tab_active');
+    }
+    
+    hideTabContent();
+    showTabContent();
+
+	tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains('tab_title')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+    });
+    
+
