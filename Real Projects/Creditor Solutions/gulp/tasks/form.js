1252b0836bb87d06ffc4f34aa81cd2f0db6089a8
yzhura <zhuravlev.yaroslav@gmail.com>
2023-03-19 17:11:20 +0200

    fix form src inject path

--- Real Projects/Creditor Solutions/gulp/tasks/form.js
+++ Real Projects/Creditor Solutions/gulp/tasks/form.js
@@ -4,9 +4,23 @@ var inject = require('gulp-inject');
 module.exports = {
     injectScripts: () => {
         const target = gulp.src('src/form.html');
-        const sources = gulp.src(['src/form/**/*.js', 'src/form/**/*.css'], {read: false});
-        return target.pipe(inject(sources))
-        .pipe(gulp.dest('src'));
+        const sources = gulp.src(['src/form/js/*.js', 'src/form/css/*.css'], {read: false});
+        return target
+          .pipe(
+            inject(sources, {
+              transform: function (filepath) {
+                if (filepath.slice(-4) === '.css') {
+                    return `<link rel="stylesheet" href=".${filepath}"/>`
+                }
+                if (filepath.slice(-3) === '.js') {
+                    return `<script src=".${filepath}"></script>`
+                }
+                // Use the default transform as fallback:
+                return inject.transform.apply(inject.transform);
+              },
+            })
+          )
+          .pipe(gulp.dest("src"));
     },
     moveFormFiles: () => {
         return gulp.src('src/form/**/*')
