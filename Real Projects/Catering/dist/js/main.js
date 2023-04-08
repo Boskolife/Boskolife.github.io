37afab0d71bc4f83325523b8ca0280a90523fae2
Boskolife <boskoboskoboskobosko@gmail.com>
2023-04-08 23:25:59 +0300

    Add faq_page

--- Real Projects/Catering/dist/js/main.js
+++ Real Projects/Catering/dist/js/main.js
@@ -1,12 +1,41 @@
 "use strict";
 
 initBurger();
+initTabs();
 
 function initBurger() {
   var burger = document.querySelector(".burger_menu");
   burger.addEventListener("click", function () {
     burger.classList.toggle("burger_active");
     document.body.classList.toggle("body_lock");
+  });
+}
+
+function initTabs() {
+  var faqTabs = document.querySelector("#faqTabs");
+  if (!faqTabs) return;
+  var tabs = document.querySelectorAll(".tab_title"),
+      tabsContent = document.querySelectorAll(".tab_content"),
+      tabsParent = document.querySelector(".tab_wrapper"),
+      closeItem = document.querySelectorAll(".open_status");
+
+  function showTabContent() {
+    var i = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+    tabsContent[i].classList.toggle("show");
+    tabs[i].classList.toggle("tab_active");
+    closeItem[i].classList.toggle("open_active");
+  }
+
+  tabsParent.addEventListener("click", function (event) {
+    var target = event.target;
+
+    if (target && target.classList.contains("tab_title")) {
+      tabs.forEach(function (item, i) {
+        if (target == item) {
+          showTabContent(i);
+        }
+      });
+    }
   });
 }
 //# sourceMappingURL=main.js.map
--- Real Projects/Catering/src/js/main.js
+++ Real Projects/Catering/src/js/main.js
@@ -1,4 +1,5 @@
 initBurger();
+initTabs();
 
 function initBurger() {
     const burger = document.querySelector(".burger_menu");
@@ -6,5 +7,33 @@ function initBurger() {
     burger.addEventListener("click", () => {
         burger.classList.toggle("burger_active");
         document.body.classList.toggle("body_lock");
+    });
+}
+
+function initTabs() {
+    const faqTabs = document.querySelector("#faqTabs");
+
+    if (!faqTabs) return;
+
+    const tabs = document.querySelectorAll(".tab_title"),
+        tabsContent = document.querySelectorAll(".tab_content"),
+        tabsParent = document.querySelector(".tab_wrapper"),
+        closeItem = document.querySelectorAll(".open_status");
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.toggle("show");
+        tabs[i].classList.toggle("tab_active");
+        closeItem[i].classList.toggle("open_active");
+    }
+
+    tabsParent.addEventListener("click", (event) => {
+        const target = event.target;
+        if (target && target.classList.contains("tab_title")) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    showTabContent(i);
+                }
+            });
+        }
     });
 }
