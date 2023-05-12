59bda1bb2a943d10510b49ee4b3a0fb6168e3ce3
Boskolife <boskoboskoboskobosko@gmail.com>
2023-05-12 15:34:49 +0300

    add tabs into case.html

--- Real Projects/Krock Digital Solutions/dist/js/main.js
+++ Real Projects/Krock Digital Solutions/dist/js/main.js
@@ -179,6 +179,24 @@ function horizontalScroll() {
     stagger: 3
   }).to({}, {
     duration: 1
-  }); // an empty tween to generate a pause at the end
+  });
+}
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active_tab", "");
+  }
+
+  document.getElementById(tabName).style.display = "grid";
+  evt.currentTarget.className += " active_tab";
 }
 //# sourceMappingURL=main.js.map
