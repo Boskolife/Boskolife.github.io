7983feacabedf199b42edeb02e5c244639b6d9c1
Boskolife <boskoboskoboskobosko@gmail.com>
2023-05-17 18:07:08 +0300

    add new sections

--- Real Projects/Bleach and Smile/dist/js/main.js
+++ Real Projects/Bleach and Smile/dist/js/main.js
@@ -1,44 +1,58 @@
 "use strict";
 
-// Swiper:
-// function destroySlidersOnResize(selector, width, obj, moreThan) {
-//     const init = {
-//       ...obj,
-//     };
-//     const win = window;
-//     const sliderSelector = document.querySelector(selector);
-//     let swiper = new Swiper(selector, init);
-//     const toggleInit = () => {
-//       const neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width
-//       if (neededWidth) {
-//         if (!sliderSelector.classList.contains("swiper-initialized")) {
-//           swiper = new Swiper(selector, init);
-//         }
-//       } else if (sliderSelector.classList.contains("swiper-initialized")) {
-//         swiper.destroy();
-//       }
-//     };
-//     ["load", "resize"].forEach((evt) =>
-//       win.addEventListener(evt, toggleInit, false)
-//     );
-// }
-// destroySlidersOnResize(".me-slider", 960, {
-//     spaceBetween: 20,
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-// });
-function initBurger() {
-  var burger = document.querySelector('.burger_wrap');
-  var menu = document.querySelector('.nav');
-  var menuContainer = document.querySelector('.menu_container');
-  burger.addEventListener('click', function () {
-    burger.classList.toggle('burger_active');
-    menu.classList.toggle('menu_active');
-    menuContainer.classList.toggle('menu_container_active');
-    document.body.classList.toggle('body_lock');
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+ininSwiper();
+initBurger();
+
+function ininSwiper() {
+  function destroySlidersOnResize(selector, width, obj, moreThan) {
+    var init = _objectSpread({}, obj);
+
+    var win = window;
+    var sliderSelector = document.querySelector(selector);
+    var swiper = new Swiper(selector, init);
+
+    var toggleInit = function toggleInit() {
+      var neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width;
+
+      if (neededWidth) {
+        if (!sliderSelector.classList.contains("swiper-initialized")) {
+          swiper = new Swiper(selector, init);
+        }
+      } else if (sliderSelector.classList.contains("swiper-initialized")) {
+        swiper.destroy();
+      }
+    };
+
+    ["load", "resize"].forEach(function (evt) {
+      return win.addEventListener(evt, toggleInit, false);
+    });
+  }
+
+  destroySlidersOnResize(".reviews_swiper", 9999, {
+    spaceBetween: 20,
+    direction: "vertical",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
   });
 }
 
-initBurger();
+function initBurger() {
+  var burger = document.querySelector(".burger_wrap");
+  var menu = document.querySelector(".nav");
+  var menuContainer = document.querySelector(".menu_container");
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("burger_active");
+    menu.classList.toggle("menu_active");
+    menuContainer.classList.toggle("menu_container_active");
+    document.body.classList.toggle("body_lock");
+  });
+}
 //# sourceMappingURL=main.js.map
--- Real Projects/Bleach and Smile/src/js/main.js
+++ Real Projects/Bleach and Smile/src/js/main.js
@@ -1,50 +1,55 @@
-// Swiper:
+ininSwiper();
+initBurger();
 
-// function destroySlidersOnResize(selector, width, obj, moreThan) {
-//     const init = {
-//       ...obj,
-//     };
-  
-//     const win = window;
-//     const sliderSelector = document.querySelector(selector);
-//     let swiper = new Swiper(selector, init);
-  
-//     const toggleInit = () => {
-//       const neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width
-//       if (neededWidth) {
-//         if (!sliderSelector.classList.contains("swiper-initialized")) {
-//           swiper = new Swiper(selector, init);
-//         }
-//       } else if (sliderSelector.classList.contains("swiper-initialized")) {
-//         swiper.destroy();
-//       }
-//     };
-  
-//     ["load", "resize"].forEach((evt) =>
-//       win.addEventListener(evt, toggleInit, false)
-//     );
-// }
+function ininSwiper() {
+    function destroySlidersOnResize(selector, width, obj, moreThan) {
+        const init = {
+            ...obj,
+        };
 
-// destroySlidersOnResize(".me-slider", 960, {
-//     spaceBetween: 20,
-  
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-// });
+        const win = window;
+        const sliderSelector = document.querySelector(selector);
+        let swiper = new Swiper(selector, init);
 
+        const toggleInit = () => {
+            const neededWidth = moreThan
+                ? win.innerWidth >= width
+                : win.innerWidth <= width;
+            if (neededWidth) {
+                if (!sliderSelector.classList.contains("swiper-initialized")) {
+                    swiper = new Swiper(selector, init);
+                }
+            } else if (
+                sliderSelector.classList.contains("swiper-initialized")
+            ) {
+                swiper.destroy();
+            }
+        };
+
+        ["load", "resize"].forEach((evt) =>
+            win.addEventListener(evt, toggleInit, false)
+        );
+    }
+
+    destroySlidersOnResize(".reviews_swiper", 9999, {
+        spaceBetween: 20,
+        direction: "vertical",
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
 
 function initBurger() {
-    const burger = document.querySelector('.burger_wrap');
-    const menu = document.querySelector('.nav');
-    const menuContainer = document.querySelector('.menu_container');
+    const burger = document.querySelector(".burger_wrap");
+    const menu = document.querySelector(".nav");
+    const menuContainer = document.querySelector(".menu_container");
 
-    burger.addEventListener('click', () =>{
-        burger.classList.toggle('burger_active');
-        menu.classList.toggle('menu_active');
-        menuContainer.classList.toggle('menu_container_active');
-        document.body.classList.toggle('body_lock');
-    })
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("burger_active");
+        menu.classList.toggle("menu_active");
+        menuContainer.classList.toggle("menu_container_active");
+        document.body.classList.toggle("body_lock");
+    });
 }
-  
-initBurger();
